5de5fd4ae085cfd5987752eb2ba95d0184a78e5f
tvallotton@uc.cl <tvallotton@uc.cl>
2021-03-09 00:04:18 -0300

    -

--- tests/api_integration_test.rs
+++ tests/api_integration_test.rs
@@ -1,8 +1,8 @@
-use cryptomkt::CryptoMktClient;
+use cryptomkt::Client;
 
 #[tokio::test]
 async fn test_api_get_markets() {
-    let api = CryptoMktClient::new("APK", "SK");
+    let api = Client::new("APK", "SK");
     let markets = api.get_markets();
     assert!(markets.await.len() > 1);
 }
