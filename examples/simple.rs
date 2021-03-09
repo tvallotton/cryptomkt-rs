4aaebe2239d5283fa9a9f5e697b67d7e4fc6ce70
tvallotton@uc.cl <tvallotton@uc.cl>
2021-03-08 21:56:29 -0300

    arreglando examples/simple

--- examples/simple.rs
+++ examples/simple.rs
@@ -10,50 +10,43 @@ const API_SECRET: &'static str = "<API SECRET>";
 
 
 
-// #[tokio::main]
-// fn main() {
-//     tokio::runtime::Builder::new_multi_thread()
-//         .enable_all()
-//         .build()
-//         .unwrap()
-//         .block_on(async {
-//     let client = CryptoMktClient::new(API_KEY, API_SECRET);
+#[tokio::main]
+async fn main() {
+    let client = CryptoMktClient::new(API_KEY, API_SECRET);
 
-//     // Get all markets available
-//     let markets = client.get_markets();
-//     for m in markets.await.iter() {
-//         println!("{}", m.get_name());
+    // Get all markets available
+    let markets = client.get_markets().await;
+    for m in markets.iter() {
+        println!("{}", m.get_name());
 
-//         // Get the current ticker for the market
-//         match m.get_current_ticker() {
-//             Ok(ticker) => {
-//                 println!("{:?}", ticker);
-//             }
-//             Err(e) => {
-//                 println!("{:?}", e);
-//             }
-//         }
+        // Get the current ticker for the market
+        match m.get_current_ticker().await {
+            Ok(ticker) => {
+                println!("{:?}", ticker);
+            }
+            Err(e) => {
+                println!("{:?}", e);
+            }
+        }
 
-//         println!("------- Orders ------");
-//         match m.get_orders_book(OrderType::Buy, 0, 20) {
-//             Ok(orders) => {
-//                 println!("{:?}", orders);
-//             }
-//             Err(e) => {
-//                 println!("{:?}", e);
-//             }
-//         }
+        println!("------- Orders ------");
+        match m.get_orders_book(OrderType::Buy, 0, 20).await {
+            Ok(orders) => {
+                println!("{:?}", orders);
+            }
+            Err(e) => {
+                println!("{:?}", e);
+            }
+        }
 
-//         println!("------- Trades ------");
-//         match m.get_trades("2018-05-15", "2018-05-16", 0, 20) {
-//             Ok(trades) => {
-//                 println!("{:?}", trades);
-//             }
-//             Err(e) => {
-//                 println!("{:?}", e);
-//             }
-//         }
-//     }})
-// }
-
-fn main() {}
+        println!("------- Trades ------");
+        match m.get_trades("2018-05-15", "2018-05-16", 0, 20).await {
+            Ok(trades) => {
+                println!("{:?}", trades);
+            }
+            Err(e) => {
+                println!("{:?}", e);
+            }
+        }
+    }
+}
